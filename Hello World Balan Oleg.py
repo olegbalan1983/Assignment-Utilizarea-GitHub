7ef084e2f40b3d94d8ee39e6a0a97105ceb2c95d
Oleg <olegbalan.ob@gmail.com>
2023-01-04 13:06:12 +0300

    Initial Code

---
+++ Hello World Balan Oleg.py
@@ -0,0 +1,14 @@
+# Se poate face prin două metode cu ajutorul buclelor:
+# 1: Cu "for"
+# 2: Cu "while"
+
+# 1: Cu "for"
+# for x in range(0,36):
+#     print("Hello World")
+# 2: Cu "while"
+# i=0
+# while(i<36):
+#     print("Hello World")
+#     i+=1
+# Sau cu "str"*"int"
+print("Hello World \n"*36)
